915f3fa4261cfba67171afc1c0b822b15a78e9eb
eliush <eliush421@gmail.com>
2018-09-25 23:29:04 +0600

    Add files via upload

--- src/components/stock.vue
+++ src/components/stock.vue
@@ -1,40 +1,34 @@
 <template>
 <div id="stock">
-    <h4>Total Stock</h4>
-    <form v-if="!show">
-        <input type="text" id="date" v-model="dates" placeholder="Date"> 
-        <input type="text" id="buy" v-model="buys" placeholder="Quantity">
+    <h4>Add in Stock</h4>
+    <form >
+        <input type="date" id="date" v-model="dates" placeholder="Date"> 
+        <input type="text" id="buy" v-model="quantitys" placeholder="Quantity">
         <button v-on:click.prevent="add">Add</button> 
     </form>
-    <div id="preview" v-if="show">
-        <ul>
-            <li v-for="stock in stocks" :key="stock.id">{{stock.date}}-{{stock.buy}}</li>
-        </ul>
-    </div>
 </div>
 </template>
 <script>
 export default {
     data() {
       return {
-        stocks:JSON.parse(localStorage.getItem("items")) || [],
-        show: false,
+        stocks: JSON.parse(localStorage.getItem("stock")) || [],
         dates: '',
-        buys: ''
+        quantitys: ''
       }
     },
     methods: {
       add: function(){
-          if(this.dates.trim().length==0 && this.buys.trim().length==0){
+          if(this.dates.trim().length==0 && this.quantitys.trim().length==0){
               alert("Its can`t be Empty");
               return
           }
           else{this.stocks.push({
               date: this.dates,
-              buy : this.buys
+              quantity : this.quantitys
           });
-          this.show = !this.show;
-          localStorage.setItem("items",JSON.stringify(this.stocks));
+          alert("Added");
+          localStorage.setItem("stock",JSON.stringify(this.stocks));
           }
       },
     }
